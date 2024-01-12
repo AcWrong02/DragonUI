6ec85263c2de59ed169bd93408c537fc5b770050
AcWrong02 <3024800887@qq.com>
2024-01-12 20:19:37 +0800

    feat(Button): [Button]add style to button component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,53 @@
 <template>
-  <Button type="primary" plain>Test Botton</Button>
-  <a href="#">TEST Link</a>
+  <h1>按钮</h1>
+  <div>
+    <Button>Default Botton</Button>
+    <Button type="primary">Primary Botton</Button>
+    <Button type="info">Info Botton</Button>
+    <Button type="danger">Danger Button</Button>
+    <Button type="success">Success Button</Button>
+    <Button type="warning">Warning Button</Button>
+  </div>
+  <div>
+    <Button plain>Default Botton</Button>
+    <Button type="primary" plain>Primary Botton</Button>
+    <Button type="info" plain>Info Botton</Button>
+    <Button type="danger" plain>Danger Button</Button>
+    <Button type="success" plain>Success Button</Button>
+    <Button type="warning" plain>Warning Button</Button>
+  </div>
+  <div>
+    <Button plain round>Default Botton</Button>
+    <Button type="primary" plain round>Primary Botton</Button>
+    <Button type="info" plain round>Info Botton</Button>
+    <Button type="danger" plain round>Danger Button</Button>
+    <Button type="success" plain round>Success Button</Button>
+    <Button type="warning" plain round>Warning Button</Button>
+  </div>
+  <div>
+    <Button plain circle>B</Button>
+    <Button type="primary" plain round circle>B</Button>
+    <Button type="info" plain round circle>B</Button>
+    <Button type="danger" plain round circle>B</Button>
+    <Button type="success" plain round circle>B</Button>
+    <Button type="warning" plain round circle>B</Button>
+  </div>
+  <div>
+    <Button plain disabled>B</Button>
+    <Button type="primary" plain disabled>B</Button>
+    <Button type="info" plain disabled>B</Button>
+    <Button type="danger" plain disabled>B</Button>
+    <Button type="success" plain disabled>B</Button>
+    <Button type="warning" plain disabled>B</Button>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Button from './components/Button/Button.vue';
+import Button from "./components/Button/Button.vue";
 </script>
 
-<style scoped></style>
+<style>
+div{
+  margin: 10px;
+}
+</style>
