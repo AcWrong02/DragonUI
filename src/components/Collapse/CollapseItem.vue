2452a7ec3602506179e9abd6e99311de91d2654b
AcWrong02 <3024800887@qq.com>
2024-01-14 13:26:34 +0800

    feat(Collapse): [Collapse]add icon to Collapse Component' header right

--- src/components/Collapse/CollapseItem.vue
+++ src/components/Collapse/CollapseItem.vue
@@ -15,6 +15,7 @@
       @click="handleClick"
     >
       <slot name="title">{{ title }}</slot>
+      <Icon icon="angle-right" class="header-angle"></Icon>
     </div>
     <Transition name="slide" v-on="transitionEvents">
       <div class="dra-collapse-item__wrapper" v-show="isActive">
@@ -29,6 +30,7 @@
 import { inject, computed } from "vue";
 import type { CollapseItemProps } from "./types";
 import { collapseContextKey } from "./types";
+import Icon from '../Icon/Icon.vue'
 defineOptions({
   name: "DraCollapseItem",
 });
