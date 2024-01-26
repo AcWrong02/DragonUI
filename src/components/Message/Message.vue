1d5eb8dc910a81cb722af322c838163ec2a1e8d7
AcWrong02 <3024800887@qq.com>
2024-01-26 22:50:32 +0800

    feat(Message): finish Message Component basic logic

---
+++ src/components/Message/Message.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="dra-message"
+    :class="{ [`dra-message--${type}`]: type, 'is-close': showClose }"
+    role="alert"
+  >
+    <div class="dra-message__content">
+        <sloe>{{ message }}</sloe>
+    </div>
+    <div class="dra-message__close" v-if="showClose">
+        <Icon icon="xmark"></Icon>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { MessageProps } from "./types";
+import Icon from '../Icon/Icon.vue'
+import { ref } from "vue";
+defineOptions({
+    name:'DraMessage'
+})
+const props = withDefaults(defineProps<MessageProps>(), {
+  duration: 3000,
+  type: "info",
+});
+
+//消息显示与否
+const visible = ref(false)
+</script>
+
+<style scoped></style>
---
+++ src/components/Message/types.ts
@@ -0,0 +1,8 @@
+import type { VNode } from "vue";
+
+export interface MessageProps {
+  message?: string | VNode;
+  duration?: number; //消息框多久会关闭，为0则不会自动关闭
+  type?: "success" | "error" | "warning" | "info";
+  showClose?: boolean; //是否显示关闭按钮
+}
