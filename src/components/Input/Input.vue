18a5cd92c7a705661bceffaa089d6c19f2966764
AcWrong02 <3024800887@qq.com>
2024-02-10 19:25:27 +0800

    feat(Input): [Input] add the basic struct of Iuput Component

---
+++ src/components/Input/Input.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="dra-input"
+    :class="{
+      [`dra-input--${type}`]: type,
+      [`dra-input--${size}`]: size,
+      'is-disabled': disabled,
+      'is-prepend': $slots.prepend,
+      'is-append': $slots.append,
+      'is-prefix': $slots.prefix,
+      'is-suffix': $slots.suffix,
+    }"
+  >
+    <!-- input -->
+    <template v-if="type !== 'textarea'">
+      <!-- prepend slot -->
+      <div v-if="$slots.prepend" class="dra-input__prepend">
+        <slot name="prepend"></slot>
+      </div>
+      <div class="dra-input__wrapper">
+        <span v-if="$slots.prefix" class="dra-input__prefix">
+          <slot name="prefix"></slot>
+        </span>
+        <input :type="type" :disabled="disabled" class="dra-input__inner" />
+      </div>
+    </template>
+    <template v-else>
+      <textarea class="dra-textarea__wrapper" :disabled="disabled"> </textarea>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { InputProps } from "./types";
+defineOptions({
+  name: "DraInput",
+});
+
+withDefaults(defineProps<InputProps>(), {
+  type: "text",
+});
+</script>
+
+<style scoped></style>
---
+++ src/components/Input/types.ts
@@ -0,0 +1,9 @@
+export interface InputProps {
+  type?: string;
+  size?: "large" | "small";
+  clearable?: boolean;
+  showPassword?: boolean;
+  disabled?: boolean;
+}
+
+
