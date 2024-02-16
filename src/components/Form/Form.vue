087b957f2d00d614c1c5655337e7db14e9c46075
AcWrong02 <3024800887@qq.com>
2024-02-16 23:58:32 +0800

    feat(Form): [Form] finish the Form and FormItem basic struct

---
+++ src/components/Form/Form.vue
@@ -0,0 +1,16 @@
+<template>
+    <form class="dra-form">
+        <slot></slot>
+    </form>
+</template>
+
+<script setup lang="ts">
+import type { FormItemProps } from "./types"
+defineOptions({
+    name:'DraForm'
+})
+
+const props = defineProps<FormItemProps>()
+</script>
+
+<style scoped></style>
---
+++ src/components/Form/FormItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dra-form-item">
+    <lebel class="dra-form-item__label">
+      <slot name="label" :label="label">{{ label }}</slot>
+    </lebel>
+    <div class="dra-form-item__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { FormItemProps } from './types';
+defineOptions({
+    name: 'DraFormItem',
+})
+
+const props = defineProps<FormItemProps>();
+</script>
+
+<style scoped></style>
