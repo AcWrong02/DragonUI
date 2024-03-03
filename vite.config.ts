22872938ec9780f069b543d0d930838853b1ccad
AcWrong02 <3024800887@qq.com>
2024-03-03 23:34:23 +0800

    finish the basic build option

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build:{
+    lib:{
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'DraElement',
+      fileName:'DragonUI'
     }
   }
 })
